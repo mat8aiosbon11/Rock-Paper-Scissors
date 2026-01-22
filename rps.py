9ddac2a75cd1fe04718ce8aaaa5bfdbca940bb9a
mat8aiosbon11 <mat8aiosbon11@gmail.com>
2026-01-22 11:02:33 +0200

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,26 @@
+import random
+print('Lets Play Rock,Paper,Scissors!')
+valid_choice = ["Rock", "Paper", "Scissors"]
+compchoice = random.choice(("Rock", "Paper", "Scissors"))
+playerchoice = input("Choose: Rock, Paper, Scissors. ")
+if playerchoice not in valid_choice:
+    print("Let's try again")
+else:
+    if compchoice == "Rock" and playerchoice == 'Paper':
+        print(' The Computer Has Selected ', compchoice)
+        print('You Win!')
+    elif compchoice == 'Rock' and playerchoice == 'Scissors':
+        print(' The Computer Has Selected', compchoice)
+        print('You Lose!')
+    elif compchoice == 'Paper' and playerchoice == 'Rock':
+        print('The Computer Has Selected', compchoice)
+        print('You lose')
+    elif compchoice == 'Paper' and playerchoice == 'Scissors':
+        print ('The Computer Has Chosen', compchoice)
+        print('You Win!')
+    elif compchoice == 'Scissors' and playerchoice == 'Rock':
+        print ('The Computer Has Chosen', compchoice)
+        print ('You Win!')
+    elif compchoice == 'Scissors' and playerchoice == 'Paper':
+        print ('The Computer Has Chosen', compchoice)
+        print ('You Lose!')
